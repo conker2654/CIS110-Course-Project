5260d84298a412e1104bc1a839c81295a99e6706
conker2654 <conker.2654@yahoo.com>
2023-02-20 12:36:40 -0800

    added first gameplay decision

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -27,3 +27,28 @@ while len(HairColor) == 0:
 EyeColor = input(f'\nWhat color are your eyes?: ')
 while len(HairColor) == 0:
     HairColor = input(f"Please enter a color for your eyes.: ")
+
+#narrative start of the story
+print('')
+print('*' * 10)
+print('*' * 10)
+print('*' * 10)
+print(f'\n*fading in from black*')
+print(f'\nYou awake in a unfamilar place that is dark, damp, and cold.')
+print(f'\nUnsure of how you arrived here you try to think about what happened.')
+print(f'Unfortunately you cannot recall anything but your name: {CharacterName}')
+
+#start of gameplay
+#Decision 1
+beginnerCave = input(f'\nIt is time to start your adventure. do you wish to leave the cave? (yes/no): ')
+while beginnerCave.lower() not in ['yes', 'no']:
+    beginnerCave = input(f'\n Please choose yes or no: ')
+if beginnerCave == 'yes':
+    print(f'\n{CharacterName} brings themself to their feet off the cold stone floor.')
+    print(f'As {CharacterName} assesses their surroundings a light shines from a corner of the cave')
+    print(f'[CharacterName} begins walking toward the light to emerge out in a dense forest')
+else:
+    print(f'\nStill dazed from awakening in this strange place {CharacterName} decides to rest for a bit')
+    print(f'After resting, {CharacterName} feel rejuvinated and ready to take on the world.')
+    print(f'With a refreshed mind {CharacterName} looks around the cave to spot a light in the corner.')
+    print(f'{characterName} begins walking toward the light to emerge out in a dense forest')
